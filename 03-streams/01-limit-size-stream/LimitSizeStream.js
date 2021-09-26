97c8cffd42ef5bb6c4826432df2cce0e638afbbe
Садырга Роман Александрович <SadyrgaRA@it.mos.ru>
2021-09-26 18:40:32 +0300

    add solution for 3-module 1-task

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,18 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.fileSizeLimit = options.limit;
+    this.writtenFileSize = 0;
   }
 
-  _transform(chunk, encoding, callback) {
+  async _transform(chunk) {
+    this.writtenFileSize = this.writtenFileSize + chunk.length;
 
+    if (this.fileSizeLimit < this.writtenFileSize) {
+      throw new LimitExceededError();
+    }
+
+    await this.push(chunk);
   }
 }
 
